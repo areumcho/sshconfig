68ef591f14ddfed5e71a4375528bfc2bf1fcda13
areumcho <cho00052@algonquinlive.com>
2019-04-02 21:41:15 -0400

    import paramiko

--- ip_file_valid.py
+++ ip_file_valid.py
@@ -1,11 +1,11 @@
 import os.path
 import sys
 
-#checking ip address file and content validity
+#Checking ip address file and content validity
 def ip_file_valid():
 
 
-    #prompting user for input and save it into the var ip_file
+    #Prompt user for input and save it into the var ip_file
     ip_file = input("\n# Enter IP file path and name eg(D:\MyApps\myfile.txt)")
 
     #Checking if the file exists (os.path is a method that is desgined to find if a file exists or not )
@@ -18,13 +18,13 @@ def ip_file_valid():
     #Open user selected file for reading(IP addresses file)
     selected_ip_file = open(ip_file, 'r')
 
-    #Starting from the beginning of the file using .seek method
+    #Start from the beginning of the file using .seek method
     selected_ip_file.seek(0)
 
-    #Reading each line (IP address) in the file
+    #Read each line (IP address) in the file
     ip_list = selected_ip_file.readlines()
 
-    #Closing the file
+    #Close the file
     selected_ip_file.close()
 
     return ip_list
--- ssh_connection.py
+++ ssh_connection.py
@@ -6,11 +6,11 @@ import re
 
 
 
-# Checking commands file
-# Prompting user for input - COMMANDS FILE
+#check commands file
+#prompt user for input - COMMANDS FILE
 cmd_file =input("\n# Enter commands file path and name (e.g. D:\MyApps\myfile.txt): ")
 
-# Verifying the validity of the COMMANDS FILE
+#verify the validity of the COMMANDS FILE
 if os.path.isfile(cmd_file) == True:
     print("\n* Command file is valid :)\n")
 else:
@@ -18,28 +18,71 @@ else:
     sys.exit()
 
 
-# Open SSHv2 connection to the device
+#open SSHv2 connection to the device
 def ssh_connection(ip):
 
-    # Import a global variables from above
-    # So using the global keyword, can modify the variable inside the function
+    #import a global variables from above
+    #using the global keyword, can modify the variable inside the function
     global user_file
     global cmd_file
 
-    # Creating SSH CONNECTION
+    #creating SSH CONNECTION
     try:
-        # Define SSH parameters
-        # opening the file for reading
+        #define SSH parameters
+        #open the file for reading
         selected_user_file = open(user_file, 'r')
 
-        # Starting from the beginning of the file
+        #start from the beginning of the file
+        selected_user_file.seek(0)
+
+        #read username from the file
+        username = selected_user_file.readlines()[0].split(',')[0].rstrip("\n")
+
+        #start from the beginning of the file
+        selected_user_file.seek(0)
+
+
+        #read the passwd from the file
+        password = selected_user_file.readline()[0].split(",")[1].rstrip("\n")
+
+
+        #log into device
+        session = paramiko.SSHClient()
+
+        #can allow or deny host keys from unknown remote servers
+        #!in production, never ever set it to autoaddpolicy!
+        #do not forget to add this before actual connection to device
+        session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        #connect to the device using uname and passwd
+        session.connect(ip.rstrip("\n"), username=username, password=password)
+
+        #start an interactive shell session on the router
+        connection = session.invoke_shell()
+
+        #set terminal length for entire output - disable pagination
+        connection.send("endable\n")
+        connection.send("terminal length 0\n")
+        time.sleep(1)
+
+        #enter global config mode
+        connection.send("\n")
+        connection.send("configure terminal\n")
+        time.sleep(1)
+
+        #open user selected file for reading
 
 
 
-# Paramiko - Python implementation of the SSHv2 protocol, providing both client and server functionality.
-# Paramiko is by far the most popular python module when it comes to remote network connections via paramiko
-# Paramiko needs to be installed since its not built in
-# python -m pip install paramiko
+
+
+
+
+
+#paramiko - Python implementation of the SSHv2 protocol, providing both client and server functionality.
+#paramiko is by far the most popular python module when it comes to remote network connections via paramiko
+#paramiko needs to be installed since its not built in
+#python -m pip install paramiko
 
 
 
